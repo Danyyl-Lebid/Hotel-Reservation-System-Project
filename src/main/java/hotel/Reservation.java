94d79180dac8324ebd00060c99d3451c73639e84
Danyyl-Lebid <64252230+Danyyl-Lebid@users.noreply.github.com>
2021-06-04 00:19:19 +0300

    Entity configuration

--- src/main/java/hotel/Reservation.java
+++ src/main/java/hotel/Reservation.java
@@ -4,27 +4,38 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Entity
+@Table(name = "reservations")
 @NoArgsConstructor
 public class Reservation {
 
+    @Id
     @Getter @Setter
+    @Column(columnDefinition = "BIGINT", nullable = false)
     private long id;
 
     @Getter @Setter
+    @Column(columnDefinition = "BIGINT", nullable = false)
     private long roomId;
 
     @Getter @Setter
+    @Column(columnDefinition = "BIGINT", nullable = false)
     private long userId;
 
     @Getter @Setter
+    @Column(columnDefinition = "DECIMAL", nullable = false)
     private double price;
 
     @Getter @Setter
+    @Column(columnDefinition = "VARCHAR", nullable = false)
+    @Enumerated(EnumType.STRING)
     private ReservationStatus status;
 
     @Getter @Setter
+    @Column(columnDefinition = "VARCHAR", nullable = false)
     private Timestamp startDate;
 
     public Reservation(long roomId, long userId, double price, ReservationStatus status, Timestamp startDate) {
